50cbe8509701cbaf07334aa907e970afb0d9fafb
Anna Levchenko <levallevchenko@gmail.com>
2021-07-09 10:24:17 +0300

    add throttle for request

--- src/store/api-actions.js
+++ src/store/api-actions.js
@@ -1,5 +1,10 @@
 import {ActionCreator} from './action';
 import axios from 'axios';
+import axiosThrottle from 'axios-throttle';
+
+const TIMEOUT_IN_MS = 300;
+
+axiosThrottle.init(axios, TIMEOUT_IN_MS);
 
 const GET_URL = 'https://rickandmortyapi.com/api/character';
 
--- src/utils.js
+++ src/utils.js
@@ -1,2 +1,16 @@
 export const capitalizeFirstLetter = (string) =>
   string.charAt(0).toUpperCase() + string.toLowerCase().slice(1);
+
+// may come in handy in the future
+export const throttle = (callback, limit) => {
+  let wait = false;
+  return function () {
+    if (!wait) {
+      callback();
+      wait = true;
+      setTimeout(() => {
+        wait = false;
+      }, limit);
+    }
+  };
+};
