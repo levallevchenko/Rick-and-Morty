93056224539684e575aad7e9b28936a5de8511b3
Anna Levchenko <levallevchenko@gmail.com>
2021-08-08 20:11:51 +0300

    rewrite app and index

---
+++ src/components/app/app.tsx
@@ -0,0 +1,4 @@
+import React, { FC } from 'react';
+import Article from '../article/article.jsx';
+
+export const App: FC = () => <Article />;
---
+++ src/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { configureStore } from '@reduxjs/toolkit';
+import reducer from './store/app/app.js';
+import { App } from './components/app/app';
+
+const store = configureStore({
+  reducer,
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(),
+});
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root'),
+);
