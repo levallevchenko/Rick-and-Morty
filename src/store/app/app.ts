241f97a846cb27854cb702943566218a03fb4a45
Anna Levchenko <levallevchenko@gmail.com>
2021-08-09 13:10:52 +0300

    add reducer types

--- src/store/app/app.ts
+++ src/store/app/app.ts
@@ -1,7 +1,21 @@
-import { createReducer } from '@reduxjs/toolkit';
-import { ActionType } from '../action-types';
+import { createReducer, PayloadAction } from '@reduxjs/toolkit';
+import { ActionType, ErrorAction, ErrorDataAction } from '../action-types';
+import { ICharacter } from '../../types/character';
 
-export const initialState = {
+export type AppStateTypes = {
+  characters: ICharacter[],
+  requestedCharacters:  ICharacter[],
+  removedCharacters: ICharacter[],
+  partyCharacters: ICharacter[],
+  partyCharacterBlocks: string[],
+  queryName: null | string,
+  searchValue: string,
+  isBadSearch: boolean,
+  error: null | ErrorAction,
+  errorData: null | ErrorDataAction,
+}
+
+export const initialState: AppStateTypes = {
   characters: [],
   requestedCharacters: [],
   removedCharacters: [],
@@ -14,36 +28,36 @@ export const initialState = {
   errorData: null,
 };
 
-export const app = createReducer(initialState, (builder) => {
+export const app = createReducer(initialState, (builder): void => {
   builder
-    .addCase(ActionType.SET_CHARACTERS, (state, action) => {
+    .addCase(ActionType.SET_CHARACTERS, (state, action: PayloadAction<ICharacter[]>) => {
       state.characters = action.payload;
     })
-    .addCase(ActionType.SET_REQUESTED_CHARACTERS, (state, action) => {
+    .addCase(ActionType.SET_REQUESTED_CHARACTERS, (state, action: PayloadAction<ICharacter[]>) => {
       state.requestedCharacters = action.payload;
     })
-    .addCase(ActionType.SET_REMOVED_CHARACTERS, (state, action) => {
+    .addCase(ActionType.SET_REMOVED_CHARACTERS, (state, action: PayloadAction<ICharacter[]>) => {
       state.removedCharacters = action.payload;
     })
-    .addCase(ActionType.SET_PARTY_CHARACTERS, (state, action) => {
+    .addCase(ActionType.SET_PARTY_CHARACTERS, (state, action: PayloadAction<ICharacter[]>) => {
       state.partyCharacters = action.payload;
     })
-    .addCase(ActionType.SET_PARTY_CHARACTER_BLOCKS, (state, action) => {
+    .addCase(ActionType.SET_PARTY_CHARACTER_BLOCKS, (state, action: PayloadAction<string[]>) => {
       state.partyCharacterBlocks = action.payload;
     })
-    .addCase(ActionType.SET_QUERY_NAME, (state, action) => {
+    .addCase(ActionType.SET_QUERY_NAME, (state, action: PayloadAction<string>) => {
       state.queryName = action.payload;
     })
-    .addCase(ActionType.SET_SEARCH_VALUE, (state, action) => {
+    .addCase(ActionType.SET_SEARCH_VALUE, (state, action: PayloadAction<string>) => {
       state.searchValue = action.payload;
     })
-    .addCase(ActionType.SET_BAD_SEARCH, (state, action) => {
+    .addCase(ActionType.SET_BAD_SEARCH, (state, action: PayloadAction<boolean>) => {
       state.isBadSearch = action.payload;
     })
-    .addCase(ActionType.SET_ERROR, (state, action) => {
+    .addCase(ActionType.SET_ERROR, (state, action: PayloadAction<ErrorAction>) => {
       state.error = action.payload;
     })
-    .addCase(ActionType.SET_ERROR_DATA, (state, action) => {
+    .addCase(ActionType.SET_ERROR_DATA, (state, action: PayloadAction<ErrorDataAction>) => {
       state.errorData = action.payload;
     });
 });
