1ac6bcd639fa727108c0830e538b0763c0b803f6
Anna Levchenko <levallevchenko@gmail.com>
2021-08-13 14:20:22 +0300

    remove unnecessary fields from character type

--- src/types/character.ts
+++ src/types/character.ts
@@ -1,22 +1,7 @@
 export interface ICharacter {
   id: number;
   name: string;
-  status: string;
-  species: string;
-  type: string;
-  gender: string;
-  origin: {
-    name: string;
-    url: string
-  };
-  location: {
-    name: string;
-    url: string
-  };
   image: string;
-  episode: string[];
-  url: string;
-  created: string;
 };
 
-export type Characters = ICharacter[]
+export type Characters = ICharacter[];
